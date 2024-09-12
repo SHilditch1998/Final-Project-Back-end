a17591315f49de60fdf335f25be5995ed0622b7e
SHilditch4177 <s.j.hilditch@outlook.com>
2024-09-12 12:25:50 +0100

    start config

--- src/HabitTracker/Controllers/Create.js
+++ src/HabitTracker/Controllers/Create.js
@@ -4,26 +4,13 @@ const Account=require("../../Users/DataBase/Model/DucklingModel")
 
 async function AddHabit(req,res) {
     try {
-        console.log(req.header("Authorization"));
-        
-        
-    const token=req.header("Authorization").replace("Bearer ","");
-    console.log(token);
-    const privatekey=process.env.JWT_KEY;
-    console.log(privatekey);
-    
-    const decodedToken=await jwt.verify(token,privatekey);
-    console.log(decodedToken);
-    
-    console.log("decoded token");
-    
-    const userEmail=decodedToken.email;
-    console.log(userEmail);
-    
-    const checkAccountExsites= await Account.findOne({where:{email:userEmail}})
-    console.log(checkAccountExsites);
-    
-
+         const token=req.header("Authorization").replace("Bearer ","");
+         const privatekey=process.env.JWT_KEY;
+         const decodedToken=await jwt.verify(token,privatekey);
+         console.log("decoded token");
+         const userEmail=decodedToken.email;
+         const checkAccountExsites= await Account.findOne({where:{email:userEmail}})
+         
         const create=await Habit.create({
            title:req.body.title,
            color:req.body.color,
